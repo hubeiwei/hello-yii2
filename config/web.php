b7a259c51eebf950b8e51355d13ae1df4052d999
hubeiwei <hubeiwei@hotmail.com>
2016-05-31 23:45:02 +0800

    Revert "Revert "删除个人邮箱配置""
    This reverts commit b77842b82786007985081a53e603f3f6c721b588.

--- config/web.php
+++ config/web.php
@@ -36,19 +36,7 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => false,
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.163.com',
-                'port' => '25',
-                'username' => 'hubeiwei1234@163.com',
-                'password' => 'Hu407519063',
-                'encryption' => 'tls',
-            ],
-            'messageConfig' => [
-                'charset' => 'UTF-8',
-                'from' => ['hubeiwei1234@163.com' => '老胡']
-            ],
+            'useFileTransport' => true,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
