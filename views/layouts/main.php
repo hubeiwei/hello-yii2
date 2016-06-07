a08fea1fd8ee47f9c551848534a055db27e15896
hubeiwei <hubeiwei@hotmail.com>
2016-06-07 17:18:29 +0800

    禁止手机端缩放网页

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,7 +19,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
