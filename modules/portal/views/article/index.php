ef46238c9d9ce01ff4a87bd825db4a2b68e8ed47
hubeiwei <hubeiwei@hotmail.com>
2016-08-07 12:40:37 +0800

    文章首页的翻页增加首页和尾页按钮

--- modules/portal/views/article/index.php
+++ modules/portal/views/article/index.php
@@ -23,32 +23,32 @@ $this->title = '文章';
 
     <div class="row">
         <div class="col-md-3 pull-right-md">
-                <?= ButtonDropdown::widget([
-                    'label' => '发布文章',
-                    'containerOptions' => [
-                        'style' => [
-                            'margin-bottom' => '15px',
-                        ],
+            <?= ButtonDropdown::widget([
+                'label' => '发布文章',
+                'containerOptions' => [
+                    'style' => [
+                        'margin-bottom' => '15px',
                     ],
-                    'options' => ['class' => 'btn-info'],
-                    'dropdown' => [
-                        'items' => [
-                            ['label' => 'Markdown', 'url' => ['create']],
-                            ['label' => 'Html', 'url' => ['create', 'type' => Article::TYPE_HTML]],
-                        ],
-                    ],
-                ]) ?>
-                <?= Collapse::widget([
+                ],
+                'options' => ['class' => 'btn-info'],
+                'dropdown' => [
                     'items' => [
-                        [
-                            'label' => '搜索文章',
-                            'content' => $this->render('_search', [
-                                'model' => $searchModel,
-                                'sort' => $dataProvider->sort,
-                            ]),
-                        ]
+                        ['label' => 'Markdown', 'url' => ['create']],
+                        ['label' => 'Html', 'url' => ['create', 'type' => Article::TYPE_HTML]],
                     ],
-                ]) ?>
+                ],
+            ]) ?>
+            <?= Collapse::widget([
+                'items' => [
+                    [
+                        'label' => '搜索文章',
+                        'content' => $this->render('_search', [
+                            'model' => $searchModel,
+                            'sort' => $dataProvider->sort,
+                        ]),
+                    ]
+                ],
+            ]) ?>
         </div>
         <div class="col-md-9">
             <div class="panel panel-default">
@@ -75,7 +75,11 @@ $this->title = '文章';
                     ?>
                 </div>
                 <div style="text-align: center;">
-                    <?= LinkPager::widget(['pagination' => $dataProvider->pagination]) ?>
+                    <?= LinkPager::widget([
+                        'pagination' => $dataProvider->pagination,
+                        'firstPageLabel' => '首页',
+                        'lastPageLabel' => '尾页',
+                    ]) ?>
                 </div>
             </div>
         </div>
