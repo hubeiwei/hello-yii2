daf1b189d8df12dcd0dae569e202e518a4d3e735
hubeiwei <hubeiwei@hotmail.com>
2016-10-17 14:03:07 +0800

    php标签内只有一个echo时使用短标签

--- views/layouts/backend.php
+++ views/layouts/backend.php
@@ -51,11 +51,9 @@ NavBar::end();
 <div class="container">
     <div class="row">
         <div class="col-md-3 col-lg-2 hidden-sm hidden-xs">
-            <?php
-            echo SideNav::widget([
+            <?= SideNav::widget([
                 'items' => $assignedMenu,
-            ]);
-            ?>
+            ]); ?>
         </div>
         <div class="col-md-9 col-lg-10">
             <?php
