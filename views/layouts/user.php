d7abf303affaa52aafa679a0bbbfabeb16559126
hubeiwei <hubeiwei@hotmail.com>
2016-11-30 11:18:07 +0800

    user布局右侧面板增加标题

--- views/layouts/user.php
+++ views/layouts/user.php
@@ -32,6 +32,11 @@ $this->beginContent('@app/views/layouts/frontend.php');
         </div>
         <div class="col-md-9">
             <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <?= $this->title ?>
+                    </h3>
+                </div>
                 <div class="panel-body">
                     <?= $content ?>
                 </div>
