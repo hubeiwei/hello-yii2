1744b0c9a890c7ad3cb16e5358cc6565c1096636
hubeiwei <hubeiwei@hotmail.com>
2018-02-02 17:00:44 +0800

    逐步规范文件的换行符，先从配置文件开始

--- environments/dev/web/index.php
+++ environments/dev/web/index.php
@@ -8,8 +8,8 @@ require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 require(__DIR__ . '/../config/bootstrap.php');
 $config = yii\helpers\ArrayHelper::merge(
-	require(__DIR__ . '/../config/main.php'),
-	require(__DIR__ . '/../config/main-local.php')
+    require(__DIR__ . '/../config/main.php'),
+    require(__DIR__ . '/../config/main-local.php')
 );
 
 (new yii\web\Application($config))->run();
